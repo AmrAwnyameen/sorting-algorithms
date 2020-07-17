bc3bcffcb757a46df176109452b8997884425597
amr.awnyameen <amr.awnyameen@gmail.com>
2020-07-17 22:46:09 +0200

    Add project files.

---
+++ BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class BubbleSort
+    {
+        //Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+        public void SortData(int [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = 1; j < arr.Length; j++)
+                    if (arr[j] < arr[j - 1])
+                        Swap(arr, j, j - 1);
+
+        }
+
+        private void Swap(int[] arr , int index1, int index2)
+        {
+            var temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort sort = new BubbleSort();
+            var arr = new int[] { 6, 2, 3, 0, 8, 4, 9, 1 };
+            sort.SortData(arr);
+            arr.ToList().ForEach(s => Console.WriteLine(s.ToString()));
+            Console.ReadLine();
+
+        }
+    }
+}
